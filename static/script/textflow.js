2d0e7d92c81fb19000db9a2f4719928f0a9fd0b7
Bibek22 <forgot.bg@hotmail.com>
2016-06-11 19:29:33 +0545

    typed strings

---
+++ static/script/textflow.js
@@ -0,0 +1,6 @@
+$(function(){
+        $(".element").typed({
+            strings: ["Hi, I'm Bibek Gautam.", "I'm a programmer and web developer from Nepal."],
+            typeSpeed: 0
+        });
+    });
