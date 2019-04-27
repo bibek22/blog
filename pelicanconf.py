e3d8efb29d699a1ee50a7016bb13e4ae07ced031
Bibek Gautam <forgot.bg@gmail.com>
2019-04-27 13:48:17 +0545

    Adds AdSense

--- pelicanconf.py
+++ pelicanconf.py
@@ -97,4 +97,14 @@ CUSTOM_CSS = 'static/custom.css'
 USE_LESS = True
 
 GOOGLE_ADSENSE = {
+    'ca_id': 'ca-pub-1726541799255911',    # Your AdSense ID
+    'page_level_ads': True,          # Allow Page Level Ads (mobile)
+    'ads': {
+        'aside': '1234561',          # Side bar banner (all pages)
+        'main_menu': '1234562',      # Banner before main menu (all pages)
+        'index_top': '1234563',      # Banner after main menu (index only)
+        'index_bottom': '1234564',   # Banner before footer (index only)
+        'article_top': '1234565',    # Banner after article title (article only)
+        'article_bottom': '1234566', # Banner after article content (article only)
+    }
 }
