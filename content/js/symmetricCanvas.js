58c8157d922ac794658252588e7883e4e2a09079
Bibek Gautam <forgot.bg@gmail.com>
2019-05-10 19:44:26 +0545

    Fixes out of canvas drawing

--- content/js/symmetricCanvas.js
+++ content/js/symmetricCanvas.js
@@ -24,21 +24,6 @@ var canvasPackaging = function(p){
         p.clearCanvas();
     }
 
-    p.mouseDragged = function(){
-        p.symmetryDraw();
-    }
-
-    p.symmetryDraw = function(){
-        // vector of point with Center as origin
-        p.px = p.createVector(p.mouseX - p.cansize/2, p.mouseY - p.cansize/2);
-        p.ppx = p.createVector(p.pmouseX - p.cansize/2, p.pmouseY - p.cansize/2);
-        for (let i = 0; i < p.symmetryAxis; i++){
-            p.line(p.cansize/2 + p.px.x, p.cansize/2 + p.px.y,
-                p.cansize/2 + p.ppx.x, p.cansize/2 + p.ppx.y,);
-            p.px.rotate(p.angle);
-            p.ppx.rotate(p.angle);
-        }
-    }
     p.clearCanvas = function(){
         p.background(240);
         p.stroke(p.symmetryFill);
@@ -52,7 +37,26 @@ var canvasPackaging = function(p){
             p.line(p.cansize/2, p.cansize/2, p.cansize/2 + p.axis.x, p.cansize/2 + p.axis.y);
             p.axis.rotate(p.angle);
         }
-        p.stroke(50);
+        p.stroke(80);
+    }
+
+
+    p.mouseDragged = function(){
+        if (p.mouseX > 0 && p.mouseX < p.cansize && p.mouseY > 0 && p.mouseY < p.cansize){
+            p.symmetryDraw();
+        }
+    }
+
+    p.symmetryDraw = function(){
+        // vector of point with Center as origin
+        p.px = p.createVector(p.mouseX - p.cansize/2, p.mouseY - p.cansize/2);
+        p.ppx = p.createVector(p.pmouseX - p.cansize/2, p.pmouseY - p.cansize/2);
+        for (let i = 0; i < p.symmetryAxis; i++){
+            p.line(p.cansize/2 + p.px.x, p.cansize/2 + p.px.y,
+                p.cansize/2 + p.ppx.x, p.cansize/2 + p.ppx.y,);
+            p.px.rotate(p.angle);
+            p.ppx.rotate(p.angle);
+        }
     }
 }
 
